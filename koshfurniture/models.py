59149e2efc8e8952ae073cc6f3347d6405497d2f
Christin John <42737168+christhelonewolf@users.noreply.github.com>
2021-02-24 22:14:08 +0530

    24-02

--- koshfurniture/models.py
+++ koshfurniture/models.py
@@ -37,6 +37,17 @@ class Customer(models.Model):
     email = models.EmailField(max_length=200, null=True)
     password = models.CharField(max_length=20, null=True)
     phone = models.IntegerField(null=True)
+
+    def register(self):
+        self.save()
+
+    def isExists(self):
+        if Customer.objects.filter(email=self.email):
+           return True
+        return False
+           
+
+        
     def __str__(self):
         return self.name
 
--- koshfurniture/views.py
+++ koshfurniture/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 from django.http import HttpResponse
+import re
 
+regex = '^[a-z0-9]+[\._]?[a-z0-9]+[@]\w+[.]\w{2,3}$'
+SpecialSym = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!#%*?&]{6,20}$"
+pat = re.compile(SpecialSym) 
 # Create your views here.
 def index(request):
     context={}
@@ -45,6 +49,50 @@ def signup(request):
         email = postDATA.get('email')
         phone = postDATA.get('Phone')
         password = postDATA.get('password')
+        #VALIDATION
 
-        print(fullname,email,phone,password)
-        return HttpResponse(request.POST)
+        value = {
+            'fullname': fullname,
+            'email': email,
+            'phone': phone,
+        }
+        
+        error_message = None
+        
+
+        customer = Customer(name=fullname,
+                                email=email,
+                                phone=phone,
+                                password=password)
+        if (not fullname):
+            error_message = "First name Required !!"
+        elif len(fullname) < 4:
+                error_message = "First name must be 4 Characters or more  "
+        elif not phone:
+            error_message   = " Phone number is required "
+        elif len(phone) < 10 or len(phone) > 10:
+            error_message = " Phone number must be 10 char Long"
+        elif not re.search(regex, email):
+            error_message = " Enter valid email id"
+
+        #PASSOWORD VALIDATION    
+        # elif len(password) < 8:
+        #     error_message = "Password should be 8 char long"
+        # elif not re.search(pat, password):
+        #     error_message="Password should contain Uppper case character, lower case character, special character and at least a number"
+        elif customer.isExists():
+            error_message = "Email address already exists"
+
+        if not error_message: 
+            print(fullname, email, phone, password)
+            
+            
+            customer.register()
+            # return redirect('store')
+            return redirect('index')
+        else:
+            data = {
+             'error': error_message,
+             'value':value
+            }
+            return render(request,'registration.html',data)

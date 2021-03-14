a705a3c1e9e2ad3d8a293a44b01b22eac0d08b07
Christin John <42737168+christhelonewolf@users.noreply.github.com>
2021-03-14 19:09:41 +0530

    postgres model complted

--- koshfurniture/models.py
+++ koshfurniture/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 import datetime
 # Create your models here.
 class Category(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    name = models.TextField(max_length=200, null=True)
     
     @staticmethod
     def get_all_categories():
@@ -13,7 +13,7 @@ class Category(models.Model):
         return self.name
 
 class Brand(models.Model):
-    name = models.CharField(max_length=200, null=True)
+    name = models.TextField(max_length=200, null=True)
     
     def __str__(self):
         return self.name
@@ -21,23 +21,23 @@ class Brand(models.Model):
 
 
 class Discount(models.Model):
-    discount = models.CharField(max_length=200, null=True)
+    discount = models.TextField(max_length=200, null=True)
 
     def __str__(self):
         return self.discount
 
 class Material(models.Model):
-    material_name = models.CharField(max_length=200, null=True)
+    material_name = models.TextField(max_length=200, null=True)
     
     def __str__(self):
         return self.material_name
 
 class Customer(models.Model):
     # user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(max_length=200, null=True)
-    password = models.CharField(max_length=20, null=True)
-    phone = models.IntegerField(null=True)
+    name = models.TextField(max_length=200, null=True)
+    email = models.TextField(max_length=200, null=True)
+    password = models.TextField(max_length=200, null=True)
+    phone = models.BigIntegerField (null=True)
 
     def register(self):
         self.save()
@@ -59,14 +59,15 @@ class Customer(models.Model):
     #     return self.name
 
 class Product(models.Model):
-    name = models.CharField(max_length=200, null=True)
-    price = models.IntegerField(default=0)
+    name = models.TextField(max_length=200, null=True)
+    price = models.BigIntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
-    Description = models.CharField(max_length=200, null=True)
+    Description = models.TextField(max_length=200, null=True)
     brand = models.ForeignKey(Brand, on_delete=models.SET_NULL , null=True ,blank=True)
     discount = models.ForeignKey(Discount, on_delete=models.SET_NULL , null=True ,blank=True)
     material = models.ForeignKey(Material, on_delete=models.SET_NULL , null=True ,blank=True)
-    image = models.ImageField(null=True,blank =True)
+    image = models.ImageField(null=True, blank=True)
+    
     
     
     @staticmethod
@@ -101,11 +102,11 @@ class Product(models.Model):
 class Order(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE,null=True)
     customer = models.ForeignKey(Customer,on_delete=models.CASCADE, null=True)
-    quantity = models.IntegerField(default=1)
-    price = models.IntegerField(default=0)
-    address = models.CharField(max_length=200, default='', blank=True)
-    pincode = models.CharField(max_length=20, default='')
-    phone = models.IntegerField(default=0, blank =True)
+    quantity = models.BigIntegerField(default=1)
+    price = models.BigIntegerField(default=0)
+    address = models.TextField(max_length=200, default='', blank=True)
+    pincode = models.TextField(max_length=200, default='')
+    phone = models.BigIntegerField(default=0, blank =True)
     date = models.DateField(default=datetime.datetime.today)
     status = models.BooleanField(default=False)
 
@@ -123,7 +124,7 @@ class Order(models.Model):
 class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True, blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, blank=True)
-    quantity = models.IntegerField(default=0, null=True, blank=True)
+    quantity = models.BigIntegerField(default=0, null=True, blank=True)
     date_added = models.DateField(auto_now_add=True)
 
     def __str__(self):
@@ -132,11 +133,11 @@ class OrderItem(models.Model):
 class ShippingAddress(models.Model):
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True,blank=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True , blank = True)
-    address = models.CharField(max_length=200, null=True)
-    city = models.CharField(max_length=200, null=True)
-    state = models.CharField(max_length=200, null=True)
-    pincode = models.CharField(max_length=200, null=True)
-    phone = models.CharField(max_length=200, null=True)
+    address = models.TextField(max_length=200, null=True)
+    city = models.TextField(max_length=200, null=True)
+    state = models.TextField(max_length=200, null=True)
+    pincode = models.TextField(max_length=200, null=True)
+    phone = models.TextField(max_length=200, null=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
@@ -144,10 +145,10 @@ class ShippingAddress(models.Model):
 
 
 class Orderplaced(models.Model):
-    price = models.IntegerField(default=0)
-    orderIds = models.CharField(max_length=30,default ='')
-    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
-    transactionid = models.CharField(max_length=200, null=True,blank=True)
+    price = models.BigIntegerField(default=0)
+    orderIds = models.TextField(max_length=200,default ='')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, null=True)
+    transactionid = models.TextField(max_length=200, null=True,blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
     def ordersave(self):
